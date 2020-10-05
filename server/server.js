afab9774c7e5a0d8c9a5927de401fbec32aa492e
k.sathurjan415@gmail.com <k.sathurjan415@gmail.com>
2020-10-05 14:40:56 +0530

    upload

--- server/server.js
+++ server/server.js
@@ -18,4 +18,4 @@ app.listen(process.env.PORT || 8000, function () {
 });
 
 // Connect to mongodb database
-mongoose.connect("mongodb://abilakshan:365463999@testcluster-shard-00-00.dzprx.mongodb.net:27017,testcluster-shard-00-01.dzprx.mongodb.net:27017,testcluster-shard-00-02.dzprx.mongodb.net:27017/funwork2?ssl=true&replicaSet=atlas-1tufku-shard-0&authSource=admin&retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology: true  });
+mongoose.connect("mongodb://abilakshan:365463999@testcluster-shard-00-00.dzprx.mongodb.net:27017,testcluster-shard-00-01.dzprx.mongodb.net:27017,testcluster-shard-00-02.dzprx.mongodb.net:27017/sathu?ssl=true&replicaSet=atlas-1tufku-shard-0&authSource=admin&retryWrites=true&w=majority",{ useNewUrlParser: true,useUnifiedTopology: true  });
